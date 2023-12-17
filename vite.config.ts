d60b7239408c80bf213dd895004ec79c3695fbcb
talant <talant.04asanov@gmail.com>
2023-12-17 23:52:38 +0600

    feat: added aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+        alias: [{ find: '@', replacement: '/src' }],
+    },
+    server: {
+        watch: {
+            usePolling: true
+        },
+        host: true,
+        port: 3000
+    },
+    preview: {
+        port: 3001
+    },
 })
