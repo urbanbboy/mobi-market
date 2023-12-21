060d069d9bd884c14ceb69546b76acf958802875
talant <talant.04asanov@gmail.com>
2023-12-22 03:03:04 +0600

    feat: show&hide password

--- src/shared/ui/Input/ui/Input.tsx
+++ src/shared/ui/Input/ui/Input.tsx
@@ -1,11 +1,12 @@
-import { InputHTMLAttributes } from "react"
-import cls from './Input.module.scss'
+import React, { useState } from 'react';
+import { InputHTMLAttributes } from 'react';
+import cls from './Input.module.scss';
 
-type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>;
 
 interface InputProps extends HTMLInputProps {
     value?: string;
-    onChange?: (value: string) => void
+    onChange?: (value: string) => void;
     label?: string;
 }
 
@@ -16,25 +17,42 @@ export const Input = (props: InputProps) => {
         onChange,
         type = 'text',
         ...otherProps
-    } = props
+    } = props;
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const inputType = type === 'password' && !showPassword ? 'password' : 'text';
 
     const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        onChange?.(e.target.value)
-    }
+        onChange?.(e.target.value);
+    };
 
     return (
         <div className={cls.group}>
             <input
                 required
-                type={type}
+                type={inputType}
                 className={cls.input}
                 value={value}
                 onChange={onChangeHandler}
                 {...otherProps}
             />
+            {type === 'password' && (
+                <div className={cls.eyeIcon} onClick={togglePasswordVisibility}>
+                    {showPassword ? (
+                        <img src={'/public/show_pass.svg'} alt="Show password" />
+                    ) : (
+                        <img src={'/public/hide_pass.svg'} alt="Hide password" />
+                    )}
+                </div>
+            )}
             <span className={cls.highlight}></span>
             <span className={cls.bar}></span>
             <label>{label}</label>
         </div>
-    )
-}
+    );
+};
