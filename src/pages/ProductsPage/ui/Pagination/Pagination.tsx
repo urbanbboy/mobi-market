920cf21837512641fcf6cae42e76f0781cfaeade
talant <talant.04asanov@gmail.com>
2023-12-26 17:58:22 +0600

    feat: pagination component

---
+++ src/pages/ProductsPage/ui/Pagination/Pagination.tsx
@@ -0,0 +1,38 @@
+import { Button, ButtonTheme } from "@shared/ui/Button";
+import cls from './Pagination.module.scss'
+
+interface PaginationProps {
+    onPageChange: (value: number) => void;
+    totalPages: number;
+    currentPage: number;
+    totalItems: number;
+
+}
+
+export const Pagination = (props: PaginationProps) => {
+    const { onPageChange, totalPages, currentPage } = props
+    const renderPageNumbers = () => {
+        const pageNumbers = []
+        for (let i = 1; i <= totalPages; i++) {
+            pageNumbers.push(
+                <Button
+                    theme={currentPage === i ? ButtonTheme.CONTAINED : ButtonTheme.DISABLED}
+                    key={i}
+                    onClick={() => onPageChange(i)}
+                    disabled={currentPage === i}
+                    active={currentPage === i}
+                >
+                    {i}
+                </Button>
+
+            )
+        }
+        return pageNumbers;
+    }
+
+    return (
+        <div className={cls.Buttons}>
+            {renderPageNumbers()}
+        </div>
+    )
+}
